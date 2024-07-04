9e1e584e074cd70db5291018521284b22495d2e0
Archibong Eyo <119775170+Archie202@users.noreply.github.com>
2024-07-04 15:35:44 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+// Add JavaScript functionality if needed
+// Example: Toggle class for menu responsiveness
+
+const navToggle = document.querySelector('.nav-toggle');
+const navMenu = document.querySelector('.nav-menu');
+
+navToggle.addEventListener('click', () => {
+  navMenu.classList.toggle('nav-menu-visible');
+});
